042836c319693ce42dc55a0f5efb43840a294560
Geraldo dos Santos <geraldo.santos@dephion.com>
2021-07-04 12:35:08 +0200

    Change page title

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Development',
+            title: 'Pascal Triangle - Visual',
         }),
     ],
     output: {
